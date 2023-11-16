0d6460d21b609f8dbf236033095a29e20bd71df4
dacsson <sda20036@gmail.com>
2023-11-16 23:37:20 +0400

    main func

--- MCTS/Program.cs
+++ MCTS/Program.cs
@@ -8,38 +8,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        //var _searcher = new MCTSSearcher();
-        //var _root_state = new BoardState();
+        var _searcher = new MCTSSearcher();
+        var _root_state = new BoardState();
 
-        //List<double> _time = new List<double>();
-        //while (!_root_state.is_finished(out int score))
-        //{
-        //    var sw = Stopwatch.StartNew();
-        //    var root_node = new Node();
-        //    var best_move = _searcher.get_best_move(root_node, _root_state, 50);
+        List<double> _time = new List<double>();
+        while (!_root_state.is_finished(out int score))
+        {
+            var sw = Stopwatch.StartNew();
+            var root_node = new Node();
+            var best_move = _searcher.get_best_move(root_node, _root_state, 50);
 
-        //    _time.Add(sw.Elapsed.TotalNanoseconds);
-        //    Console.WriteLine($"Time used: {sw.Elapsed}");
+            _time.Add(sw.Elapsed.TotalNanoseconds);
+            Console.WriteLine($"Time used: {sw.Elapsed}");
 
-        //    _root_state.play(best_move);
+            _root_state.play(best_move);
 
-        //    Console.WriteLine(_root_state);
+            Console.WriteLine(_root_state);
 
-        //}
+        }
 
-        //Console.WriteLine($"\n who won? {_time.Average()} ");
+        Console.WriteLine($"\n who won? {_time.Average()} ");
 
         //var summary = BenchmarkRunner.Run<MCTSSearcher>();
 
-        var _runner = new MCTSRunner('x');
-        var str = "xxxx____o_o_o_o__________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________";
-        BoardState state = (BoardState)_runner.generate_bitboard(str);
-        int score = 0;
-        Console.WriteLine(state.is_finished(out score));
-        Console.WriteLine(state);
+        //var _runner = new MCTSRunner('x');
+        //var str = "xxxx____o_o_o_o__________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________";
+        //BoardState state = (BoardState)_runner.generate_bitboard(str);
+        //int score = 0;
+        //Console.WriteLine(state.is_finished(out score));
+        //Console.WriteLine(state);
 
-        var sw = Stopwatch.StartNew();
-        Console.WriteLine(_runner.Run(str));
-        Console.WriteLine($"Time used: {sw.Elapsed}");
+        //var sw = Stopwatch.StartNew();
+        //Console.WriteLine(_runner.Run(str));
+        //Console.WriteLine($"Time used: {sw.Elapsed}");
     }
 }
